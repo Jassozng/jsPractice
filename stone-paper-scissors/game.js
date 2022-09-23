dafaa12a2789eaf7791e166b27bdddfdbf6e25aa
jasso <jassozng@gmail.com>
2022-09-22 22:53:57 -0500

    stone-papar-scissors game done

--- stone-paper-scissors/game.js
+++ stone-paper-scissors/game.js
@@ -1,6 +1,35 @@
-const playerSelection = "Stone";
+window.onload = () => {
+  document.querySelectorAll('.playerSelection').forEach(item => {
+      item.addEventListener('click', event => {
+          const gameResults = startGame(item.value);
+          const gameForm = document.getElementById("game-form");
+          gameForm.innerHTML = 
+          `
+          <fieldset styles="flex-direction: column">
+            <legend>Game Results</legend>
+            <p class="result">Your chooise</p><br>
+            <img class="playerSelection" src="./resource/${gameResults["playerSelection"]}.png">
+            <p class="result">Computer chooise</p>
+            <img class="playerSelection" src="./resource/${gameResults["computerSelection"]}.png">
+            <p class="result">
+              Result: ${gameResults["result"]}
+            </p><br>
+            <a href="index.html" class="result" >Play Again
+          </fieldset>
+          `;
+      });
+  });
+};
 
-const computerSelection = () => {
+
+function startGame(playerSelection){
+  const computerSelection = generateComputerSelection();
+  const result = winnerDefiner(playerSelection, computerSelection);
+  const gameResults = {"playerSelection" : playerSelection, "computerSelection": computerSelection, "result": result };
+  return gameResults;
+}
+
+const generateComputerSelection = () => {
   const posibleSelections = ["Stone", "Scissor", "Paper"];
   const randomSelection = Math.floor(Math.random() * posibleSelections.length);
   return posibleSelections[randomSelection];
@@ -9,11 +38,9 @@ const computerSelection = () => {
 const winnerDefiner = (playerSelection, computerSelection) => {
   const gameResults = {
     "Stone": stoneComparations(computerSelection), 
-    "Scissors": scissorComparations(computerSelection),
+    "Scissor": scissorComparations(computerSelection),
     "Paper": paperComparations(computerSelection)
   };
-	console.log(playerSelection);
-  console.log(computerSelection);
   return gameResults[playerSelection];
 };
 
@@ -23,7 +50,7 @@ const stoneComparations = (computerSelection) => {
       return "Win";
     case "Paper":
       return "Lose";
-    default: 
+    case "Stone": 
       return "Tie";
   }
 };
@@ -34,7 +61,7 @@ const scissorComparations = (computerSelection) => {
       return "Win";
     case "Stone":
       return "Lose";
-    default: 
+    case "Scissor":
       return "Tie";
   }
 };
@@ -44,10 +71,7 @@ const paperComparations = (computerSelection) => {
       return "Win";
     case "Scissor":
       return "Lose";
-    default: 
+    case "Paper":
       return "Tie";
   }
 };
-
-
-winnerDefiner(playerSelection, computerSelection());
